c94dca5566559bcfe3a40a5674e67e3bb09277d1
Bence Gallai <bgg203@exeter.ac.uk>
2026-02-13 12:22:11 +0000

    Add CopyArrayApp, chmod test_all.sh

---
+++ CopyArrayApp.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class CopyArrayApp{
+    public static void main(String[] args) {
+        int nVals = args.length;
+        int[] myVals = new int[nVals];
+        for (int i = 0; i < nVals; i++) {
+            myVals[i] = Integer.parseInt(args[i]);
+        }
+        System.out.println("original values: " + Arrays.toString(myVals));
+        
+        ArrayList<Integer> uniqueVals = new ArrayList<>();
+        for (int n : myVals) {
+            if (!uniqueVals.contains(n)) {
+                uniqueVals.add(n);
+            }
+        }
+        System.out.println("unique values: " + uniqueVals.toString()t);
+    }
+}
